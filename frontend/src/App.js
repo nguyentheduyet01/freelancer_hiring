aa0c393ad8818cc824b7bfc3ffd6e43f0b2e6006
DESKTOP-01C8P5I\THEVU <thevu2468@gmail.com>
2023-03-28 22:37:35 +0700

    add dropdown on header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
 
 function App() {
   return (
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,72 +1,96 @@
-import React from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
-import { Search } from 'react-bootstrap-icons';
-// import SearchIcon from '@mui/icons-material/Search';
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { Search, ChevronDown } from "react-bootstrap-icons";
+import DDFreelance from "../dropdown/DDFreelance";
+import "./Header.css";
 const Header = () => {
+  const [isShow, setIsShow] = useState(false);
+
   return (
     <>
-      <Row style={{ borderBottom: '1px #BECCBE solid', lineHeight: '70px' }}>
-        <Col xs xm lg='2'>
-          <div className='logo'>hello</div>
-        </Col>
-        <Col xs xm lg='4'>
-          <Row>
-            <Col xm lg='5'>
-              Tìm kiếm việc làm
-            </Col>
-            <Col xm lg='5'>
-              Tuyển dụng
-            </Col>
-          </Row>
-        </Col>
-        <Col xs xm lg='4'>
-          <Row className='mt-3'>
-            <Col xm lg='3'></Col>
-            <Col style={{ padding: '15  px 0 0 0' }}>
-              <Form style={{ width: '90%', position: 'relative' }}>
-                <Form.Group className='mb-3' controlId='formBasicEmail'>
-                  <Form.Control type='text' placeholder='search' style={{ fontSize: '110%' }} />
-                </Form.Group>
-                <Button
-                  type='submit'
-                  className='buttonSubmit'
-                  style={{
-                    backgroundColor: 'transparent',
-                    width: '35px',
-                    height: '35px',
-                    position: 'absolute',
-                    top: '3px',
-                    right: '0',
-                    border: 'none',
-                  }}
+      <div style={{ borderBottom: "1px #BECCBE solid" }}>
+        <Row style={{ width: "95%", margin: "0 auto", lineHeight: "70px" }}>
+          <Col xs xm lg='2'>
+            <div className='logo'>hello</div>
+          </Col>
+          <Col xs xm lg='4'>
+            <Row>
+              <Col xm lg='5'>
+                <span
+                  className='menu-item'
+                  onMouseLeave={() => setIsShow(() => false)}
+                  onMouseEnter={() => setIsShow(() => true)}
                 >
-                  <Search
-                    size={25}
-                    color='#5a645a'
-                    style={{ position: 'absolute', top: '5px', right: '5' }}
-                  />
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Col>
-        <Col
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Row>
-            <Col>
-              <Button variant='light'>Login</Button>
-            </Col>
-            <Col>
-              <Button variant='success'>Register</Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+                  Tìm kiếm việc làm
+                  {isShow && <DDFreelance />}
+                </span>
+                <span>
+                  <ChevronDown size={12} />
+                </span>
+              </Col>
+              <Col xm lg='5'>
+                <span
+                  className='menu-item'
+                  // onMouseLeave={() => setIsShow(() => false)}
+                  // onMouseEnter={() => setIsShow(() => true)}
+                >
+                  Tuyển dụng
+                </span>
+                <span>
+                  <ChevronDown size={12} />
+                </span>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs xm lg='4'>
+            <Row className='mt-3'>
+              <Col xm lg='3'></Col>
+              <Col style={{ padding: "15  px 0 0 0" }}>
+                <Form style={{ width: "90%", position: "relative" }}>
+                  <Form.Group className='mb-3' controlId='formBasicEmail'>
+                    <Form.Control type='text' placeholder='search' style={{ fontSize: "110%" }} />
+                  </Form.Group>
+                  <Button
+                    type='submit'
+                    className='buttonSubmit'
+                    style={{
+                      backgroundColor: "transparent",
+                      width: "35px",
+                      height: "35px",
+                      position: "absolute",
+                      top: "3px",
+                      right: "0",
+                      border: "none",
+                    }}
+                  >
+                    <Search
+                      size={25}
+                      color='#5a645a'
+                      style={{ position: "absolute", top: "5px", right: "5" }}
+                    />
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Row>
+              <Col>
+                <Button variant='light'>Login</Button>
+              </Col>
+              <Col>
+                <Button variant='success'>Register</Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
