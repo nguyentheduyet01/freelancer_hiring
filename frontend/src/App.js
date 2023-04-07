c0ae1d5a242206d2d6c4452deb5bf17677d03db6
DESKTOP-01C8P5I\THEVU <thevu2468@gmail.com>
2023-04-07 23:43:08 +0700

    add banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
-
+import Login from "./pages/login/Login";
+import Slider from "./components/slider/Slider";
 function App() {
   return (
     <div>
       <Header />
+      <Login />
+      {/* <Slider /> */}
       <Routes>
         <Route path='/'></Route>
       </Routes>
