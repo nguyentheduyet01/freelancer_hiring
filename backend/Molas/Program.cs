83bd4e072992348d483cb7e821c90adcae1743f8
nguyentheduyet01 <theduyet.138@gmail.com>
2023-04-30 21:26:14 +0700

    cors

--- backend/Molas/Program.cs
+++ backend/Molas/Program.cs
@@ -34,7 +34,6 @@ builder.Services.AddDbContext<Molas.Molas.MolasDbContext>(
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
