15b34e70df3540a5b0f94473ae2a77aa987e01e6
DESKTOP-01C8P5I\THEVU <nguyenthevu2602@gmail.com>
2023-04-30 21:49:31 +0700

    store

--- frontend/src/reducer/store.js
+++ frontend/src/reducer/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./slice/accountSlice";
 
+const rootReducer = combineReducers({
+  account: accountReducer,
+});
+
 const store = configureStore({
-  reducer: {
-    account: accountReducer,
-  },
+  reducer: rootReducer,
 });
 
 export default store;
