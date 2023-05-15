a3db53fb066cbc9e8c6ff1d58814814bec6aee8e
nguyentheduyet01 <theduyet.138@gmail.com>
2023-05-15 23:46:22 +0700

    backup data

---
+++ backend/Molas/DTO/UserPostDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Molas.DTO
+{
+    public class UserPostDTO
+    {
+        public decimal? Salary { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public int? UserId { get; set; }
+
+        public int? PostId { get; set; }
+        public int? CvId { get; set; }
+        public string? Suggestion { get; set; }
+        public DateTime? IntendTime { get; set; }
+    }
+}
