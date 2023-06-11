a92841e4495892dd50d17e10231093e822e187ee
DESKTOP-01C8P5I\THEVU <nguyenthevu2602@gmail.com>
2023-06-11 23:04:43 +0700

    cv

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -43,10 +43,10 @@ const Footer = () => {
                   <Link className='linkFooter'>Hướng dẫn freelancer kiếm tiền </Link>
                 </li>
                 <li>
-                  <Link className='linkFooter'>HƯớng dẫn freelancer hoàn thiện hồ sơ </Link>
+                  <Link className='linkFooter'>Hướng dẫn freelancer hoàn thiện hồ sơ </Link>
                 </li>
                 <li>
-                  <Link className='linkFooter'>HƯớng dẫn chào giá dự án</Link>
+                  <Link className='linkFooter'>Hướng dẫn chào giá dự án</Link>
                 </li>
                 <li>
                   <Link className='linkFooter'>Hướng dẫn tạo gói dịch vụ</Link>
@@ -76,9 +76,15 @@ const Footer = () => {
             </div>
           </Col>
           <Col>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5400.1820424054895!2d105.80328354060212!3d21.02844562269753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab424a50fff9%3A0xbe3a7f3670c0a45f!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBHaWFvIFRow7RuZyBW4bqtbiBU4bqjaQ!5e0!3m2!1svi!2s!4v1685984451567!5m2!1svi!2s" width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe
+              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5400.1820424054895!2d105.80328354060212!3d21.02844562269753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab424a50fff9%3A0xbe3a7f3670c0a45f!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBHaWFvIFRow7RuZyBW4bqtbiBU4bqjaQ!5e0!3m2!1svi!2s!4v1685984451567!5m2!1svi!2s'
+              width='400'
+              height='300'
+              allowfullscreen=''
+              loading='lazy'
+              referrerpolicy='no-referrer-when-downgrade'
+            ></iframe>
           </Col>
-
         </Row>
         <Row className='mt-4'>
           <Col xm='3' lg='3'>
@@ -102,7 +108,9 @@ const Footer = () => {
           </Col>
         </Row>
         <hr />
-        <p style={{ fontSize: "12px", textAlign: "center" }}>© 2019 - 2023 Molas® Đồ án ra trường.</p>
+        <p style={{ fontSize: "12px", textAlign: "center" }}>
+          © 2019 - 2023 Molas® Đồ án ra trường.
+        </p>
       </Container>
     </footer>
   );
